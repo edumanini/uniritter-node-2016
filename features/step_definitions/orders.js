b81949688a30be6054e88ec34cfde98ce554b976
Diogo Lucas <diogo.lucas@gmail.com>
2016-06-18 04:20:17 +0000

    Adds further /order tests

--- features/step_definitions/orders.js
+++ features/step_definitions/orders.js
@@ -1,11 +1,9 @@
 module.exports = function () {
     const 
+        _ = require('lodash'),
         chai = require('chai'),
-        expect = chai.expect;
-    chai.use(require('chai-subset'));
-
-    this.Given(/^a valid order$/, function () {
-        this.payload = {
+        expect = chai.expect,
+        sample_payload = {
             "data": {
                 "type": "orders",
                 "attributes": {
@@ -15,7 +13,12 @@ module.exports = function () {
                     ]
                 }
             }
-        }
+        };
+        
+    chai.use(require('chai-subset'));
+
+    this.Given(/^a valid order$/, function () {
+        this.payload = sample_payload;
         this.verb = 'post';
         this.endpoint = 'orders';
         this.expected = { statusCode: 201 };
@@ -30,11 +33,68 @@ module.exports = function () {
         });
     });
 
+    this.Given(/^an order in a 'new' status$/, function () {
+        const that = this;
+        var payload = _.clone(sample_payload);
+        payload.data.attributes.status = 'new';
+        return this.doHttpRequest('orders', 'post', payload)
+        .then(result => that.existingOrder = result.body);
+
+    });
+
     this.Then(/^I receive a success message$/, function () {
         expect(this.response.statusCode).to.equal(this.expected.statusCode);
     });
     
     this.Then(/^the new order id$/, function() {
         expect(this.response.body.data.id).to.not.be.undefined;
-    })
+    });
+  
+    this.When(/^I ask to cancel it$/, function (callback) {
+      // Write code here that turns the phrase above into concrete actions
+      callback(null, 'pending');
+    });
+    
+    this.Then(/^my order status turns to 'cancelled'$/, function (callback) {
+      // Write code here that turns the phrase above into concrete actions
+      callback(null, 'pending');
+    });
+    
+    this.Then(/^a message saying that (.*)$/, function (notification, callback) {
+      // Write code here that turns the phrase above into concrete actions
+      callback(null, 'pending');
+    });
+    
+    this.Then(/^I receive an error response$/, function (callback) {
+      // Write code here that turns the phrase above into concrete actions
+      callback(null, 'pending');
+    });
+    
+    this.Given(/^an invalid order that (.*)$/, function (condition, callback) {
+      // Write code here that turns the phrase above into concrete actions
+      callback(null, 'pending');
+    });
+    
+   this.Given(/^an existing order with a (.*) status$/, function (status) {
+        const that = this;
+        var payload = _.clone(sample_payload);
+        payload.data.attributes.status = status;
+        return this.doHttpRequest('orders', 'post', payload)
+        .then(result => that.existingOrder = result.body);
+   });
+
+   this.When(/^I search this order$/, function () {
+        const that = this;
+        return this.doHttpRequest('orders/' + this.existingOrder.data.id, 'get', undefined)
+        .then(result => that.response = JSON.parse(result.body));
+   });
+
+   this.Then(/^I receive the order data$/, function () {
+       expect(this.response.data).to.not.be.undefined;
+   });
+
+   this.Then(/^its status is (.*)$/, function (s) {
+       expect(this.response.data.attributes.status).to.equal(s);
+   });
+
 }
