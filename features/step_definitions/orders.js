33dbb6a29c244075f5267ac170b5490330e25b5b
Eduardo Pletsch Manini <edumanini@gmail.com>
2016-07-21 02:12:52 +0000

    * copy orders to master working tree

--- features/step_definitions/orders.js
+++ features/step_definitions/orders.js
@@ -16,7 +16,6 @@ module.exports = function () {
                 }
             }
         
-
         return this.doHttpRequest('orders', 'post', payload)
         .then((response) => {
             that.existingOrder = response.body;
@@ -43,8 +42,93 @@ module.exports = function () {
         expect(this.responseBody.data.attributes.status).to.equal(status);
     });
     
+    this.Given(/^a valid order$/, function () {
+        this.payload = {
+            data: {
+                type: 'orders',
+                attributes: {
+                    items: [{ product_id: '598b04ea-8c20-4240-9c2b-1d36350a8d33', quantity: 1}]
+                    }
+                }
+            }
+    });
+    
+    this.When(/^I submit it to the API$/, function () {
+        const 
+            that = this;
+        return this.doHttpRequest('orders', 'post', that.payload)
+        .then((response) => {
+            that.responseBody = response.body;
+            return response;
+        })
+        .catch(error => {
+            that.error = error;
+            return error;
+        })
+    });
+    
+    this.Then(/^I receive a success message$/, function () {
+        expect(this.responseBody.data).not.to.be.undefined;
+    });
+    
+    this.Then(/^the new order id$/, function () {
+        expect(this.responseBody.data.id).not.to.be.undefined;
+    });
+    
+    
+    this.Given(/^an invalid order that (.*)$/, function (condition) {
+        if(condition=="is missing an item quantity")
+        {
+            this.payload = {
+                data: {
+                    type: 'orders',
+                    attributes: {
+                        items: [{ product_id: '598b04ea-8c20-4240-9c2b-1d36350a8d33', quantity: 0}]
+                        }
+                    }
+                };
+        }
+        if(condition=="has an invalid format in product_id")
+        {
+           this.payload = {
+                data: {
+                    type: 'orders',
+                    attributes: {
+                        items: [{ product_id: 'xxx', quantity: 1}]
+                        }
+                    }
+                };
+        }
+    });
+    
+   this.Then(/^I receive an error response$/, function () {
+    	expect(this.error.body).not.to.be.undefined;
+    });
+
+    this.Then(/^a message saying that (.*)$/, function (notification) {
+        if(notification=="item.quantity is mandatory") {
+	        expect(this.error.body.errors[0].validation.keys[0]).to.equal("data.attributes.items.0.quantity");
+        }
+        if(notification=="product_id must be a uuid") {
+	        expect(this.error.body.errors[0].validation.keys[0]).to.equal("data.attributes.items.0.product_id");
+        }
+    });
+
     this.Then(/^wait a few seconds$/, function (callback) {
         setTimeout(callback, 3000);
+    });
+    
+    this.Then(/^it moves to a paid status$/, function () {
+        const 
+            that = this,
+            id = that.responseBody.data.id;
+       
+        return this.doHttpRequest('orders/' + id, 'get', undefined)
+        .then((response) => {
+            that.responseBody = JSON.parse(response.body);
+            expect(that.responseBody.data.attributes.status).to.equal("paid");
+        });
+        
     });
     
 }
