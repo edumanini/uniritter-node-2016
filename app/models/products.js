1d805cfbdf140abab9e1bce8eceeea3f815ee42b
Eduardo Pletsch Manini <edumanini@gmail.com>
2016-07-26 01:23:08 +0000

    Implementado endpois products

---
+++ app/models/products.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const 
+    Types = require('joi'),
+    uuid = require('uuid'),
+    eh = require('../utils/eventhandling'),
+    _ = require('lodash');
+
+module.exports = function (server) {
+    const 
+        harvesterPlugin = server.plugins['hapi-harvester'],
+        schema = {
+            type: 'products',
+            attributes: {
+                name: Types.string(),
+                price: Types.number(),
+                brand: Types.string(),
+                model: Types.string()
+            }
+        }
+
+    
+    server.route(harvesterPlugin.routes['get'](schema));
+    server.route(harvesterPlugin.routes['getById'](schema));
+    
+    var post = _.clone(harvesterPlugin.routes['post'](schema));
+    //hack: this is chockfull of state. No biggie, since it's a PoC. But please don't do this in prod code.
+   
+    server.route(post);
+}
